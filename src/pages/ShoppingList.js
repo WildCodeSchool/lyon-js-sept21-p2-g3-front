39c93fb7fc8cc0cd809420468a8e95dd0f432751
Dr Wouse <e.gon66@outlook.fr>
2021-10-23 15:49:24 +0200

    Shopping List in construction + Add ComponentListOfProduct for ShoppingList + Installation MUI

--- src/pages/ShoppingList.js
+++ src/pages/ShoppingList.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import ListOfProducts from '../components/ListOfProducts';
 
 function ShoppingList() {
-  return <h1>ShoppingList</h1>;
+  return (
+    <>
+      <div className="flex-auto text-xl font-semibold justify-items-center text-center">
+        <h1 className="text-3xl p-2">Mon panier</h1>
+
+        <h2 className="p-2 my-2 border-t-2 border-b-2 border-red-200">
+          BOUCHERIE
+        </h2>
+
+        <ListOfProducts />
+
+        <h2 className="p-2 my-2 border-t-2 border-b-2 border-red-200">
+          POISSONERIE
+        </h2>
+
+        <ListOfProducts />
+
+        <h2 className="p-2 my-2 border-t-2 border-b-2 border-red-200">
+          PRODUITS FRAIS
+        </h2>
+
+        <ListOfProducts />
+
+        <h2 className="p-2 my-2 border-t-2 border-b-2 border-red-200">
+          FRUITS
+        </h2>
+
+        <ListOfProducts />
+
+        <h2 className="p-2 my-2 border-t-2 border-b-2 border-red-200">
+          LÉGUMES
+        </h2>
+
+        <ListOfProducts />
+
+        <h2 className="p-2 my-2 border-t-2 border-b-2 border-red-200">
+          ÉPICERIES
+        </h2>
+
+        <ListOfProducts />
+
+        <h2 className="p-2 my-2 border-t-2 border-b-2 border-red-200">
+          BOISSONS
+        </h2>
+
+        <ListOfProducts />
+      </div>
+    </>
+  );
 }
 
 export default ShoppingList;
