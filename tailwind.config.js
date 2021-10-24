b63b37315b83c4fc3218e19e1fbe2aadb508e2fb
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-10-24 11:28:49 +0200

    CSS of the Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      primary: '#157A2C',
+      secondary: '#7CC156',
+      background: '#F3F4DF',
+      third: '#F4A871',
+    },
   },
   variants: {
     extend: {},
