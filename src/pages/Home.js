19fe4798c26c3c684224c7dd9602888f067dc516
Yannis <yannis.barba.dev@gmail.com>
2021-10-25 16:56:23 +0200

    added a full screen height for the component Home thanks to flex-grow: 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import axios from 'axios';
 import RecipeTile from '../components/RecipeTile';
 
 function Home() {
-  const [label, setLabel] = useState();
-  const [imgSrc, setimgSrc] = useState();
+  // construction of the arrays with the wanted informations from the api for the app
+  const [recipes, setRecipes] = useState({
+    labels: [],
+    imgSrcs: [],
+  });
   const getRecipe = () => {
     axios
 
@@ -19,25 +23,47 @@ function Home() {
       // Use this data to update the state
 
       .then((data) => {
-        console.log(data.hits[0].recipe.label);
-        setLabel(data.hits[0].recipe.label);
-        setimgSrc(data.hits[0].recipe.image);
+        console.log(data);
+        // console.log(data.hits[0].recipe.label);
+        // console.log(data.hits.length);
+        const newLabels = [];
+        const newImgSrcs = [];
+        for (
+          let recipeIndex = 0;
+          recipeIndex < data.hits.length;
+          recipeIndex++
+        ) {
+          // data.hits contains all of the different recipes existing according to the input of the user
+          newLabels.push(data.hits[recipeIndex].recipe.label);
+          newImgSrcs.push(data.hits[recipeIndex].recipe.image);
+          console.log(newLabels);
+        }
+        setRecipes({
+          labels: newLabels,
+          imgSrcs: newImgSrcs,
+        });
       });
   };
 
   return (
     <div
       id="Home"
-      className="flex flex-col justify-center items-center gap-10 bg-primary"
+      className="flex flex-col justify-center items-center gap-10 bg-primary flex-grow"
     >
       <h1 className="text-center">Home</h1>
       <button type="button" onClick={getRecipe}>
         {' '}
         New Recipe{' '}
       </button>
-      <RecipeTile imgSrc={imgSrc} imgAlt={label} />
-      <RecipeTile imgSrc="https://bit.ly/2GJFu5W" imgAlt="Salade Caesar" />
-      <RecipeTile imgSrc="https://bit.ly/2GJFu5W" imgAlt="Salade Caesar" />
+      {recipes.labels.map((recipe, index) => {
+        return (
+          <RecipeTile
+            key={recipe}
+            imgAlt={recipe}
+            imgSrc={recipes.imgSrcs[index]}
+          />
+        );
+      })}
     </div>
   );
 }
