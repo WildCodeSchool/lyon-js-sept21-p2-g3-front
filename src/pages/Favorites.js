bb4b6a6d18dd7ba32ff8ea492ec681e9d4351dee
Yannis <yannis.barba.dev@gmail.com>
2021-10-27 23:02:53 +0200

    create the requests to the edamam API

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,27 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import RecipeTile from '../components/RecipeTile';
 // import RecipeTile from '../components/RecipeTile';
 
 function Favorites() {
-  const [favorites, setFavorites] = useState([]);
-  const getFavorites = () => {
+  const [favoritesId, setFavoritesId] = useState([]);
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const getFavoritesId = () => {
     axios
       .get('http://localhost:5000/favorites')
       .then((response) => response.data)
       .then((data) => {
         console.log(data);
-        setFavorites(data);
-        console.log(favorites);
+        setFavoritesId(data);
+        console.log(favoritesId);
       });
   };
+
+  const getFavorites = (favoriteId) => {
+    axios
+      .get(
+        ` https://api.edamam.com/api/recipes/v2/%23${favoriteId}?type=public&app_id=f3601de5&app_key=960c7d96572cfedbc3eb6bffbfaf24c9`
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        console.log('data : ', data);
+        console.log('data[0] : ', data[0]);
+        setFavoriteRecipes(favoriteRecipes.push(data));
+        console.log('favoriteRecipes : ', favoriteRecipes);
+      });
+  };
+
+  useEffect(() => {
+    for (
+      let favoriteIndex = 0;
+      favoriteIndex < favoritesId.length;
+      favoriteIndex += 1
+    ) {
+      getFavorites(favoritesId[favoriteIndex]);
+    }
+  }, [favoritesId]);
 
   return (
     <div>
       <h1> Favorites</h1>
-      <button type="button" onClick={getFavorites}>
+      <button type="button" onClick={getFavoritesId}>
         {' '}
-        Get Favorites{' '}
+        Get Favorites Id{' '}
       </button>
+      {favoriteRecipes.map((favorite) => {
+        return (
+          <RecipeTile
+            imgSrc={favorite.recipe.image}
+            imgAlt={favorite.recipe.label}
+          />
+        );
+      })}
     </div>
   );
 }
