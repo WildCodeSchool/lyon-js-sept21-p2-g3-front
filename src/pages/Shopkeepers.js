1637d4c40d3d273479ccb692548c2c8601b80c06
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-10-27 17:01:45 +0200

    Modified map to the Shopkeepers

--- src/pages/Shopkeepers.js
+++ src/pages/Shopkeepers.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function Shopkeepers() {
-  return <h1>Shopkeepers</h1>;
+  return (
+    <>
+      <div className="flex-auto text-xl font-semibold justify-items-center text-center">
+        <h1 className="p-2 my-2 border-t-4 border-b-4 border-background bg-background bg-opacity-60">
+          Find your ShoopKeepers
+        </h1>
+        <MapContainer
+          className="h-80"
+          id="mapid"
+          center={[45.764043, 4.835659]}
+          zoom={12}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </>
+  );
 }
 
 export default Shopkeepers;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+/* eslint-disable */
+// const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
+
   theme: {
     colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
       primary: '#157A2C',
       secondary: '#7CC156',
-      background: '#F3F4DF',
+      background: '#FECACA',
       third: '#F4A871',
     },
   },
