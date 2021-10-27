92db6228e624b7b0b1f1741c2484206d387e16c3
Yannis <yannis.barba.dev@gmail.com>
2021-10-27 22:37:22 +0200

    create an axios request to the local server in order to get the favorites recipes

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+// import RecipeTile from '../components/RecipeTile';
 
 function Favorites() {
-  return <h1>Favori</h1>;
+  const [favorites, setFavorites] = useState([]);
+  const getFavorites = () => {
+    axios
+      .get('http://localhost:5000/favorites')
+      .then((response) => response.data)
+      .then((data) => {
+        console.log(data);
+        setFavorites(data);
+        console.log(favorites);
+      });
+  };
+
+  return (
+    <div>
+      <h1> Favorites</h1>
+      <button type="button" onClick={getFavorites}>
+        {' '}
+        Get Favorites{' '}
+      </button>
+    </div>
+  );
 }
 
 export default Favorites;
