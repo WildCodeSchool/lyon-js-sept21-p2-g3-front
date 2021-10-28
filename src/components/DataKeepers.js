b560cf375ad8b74f970113cdeb9332e9bcad0273
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-10-28 12:28:58 +0200

    included the keepers and the points of the map

---
+++ src/components/DataKeepers.js
@@ -0,0 +1,37 @@
+const dataKeepers = {
+  cardData: [
+    {
+      id: 1,
+      img: '/media/prodmen.png',
+      title: 'Les Jardins du Plateau',
+      description: 'Producteur de légumes',
+      adress: '800 route des Echets, 69270 CAILOUX SUR FONTAINES',
+      gps: '45.85147, 4.87585',
+    },
+    {
+      id: 1,
+      img: '/media/prodwoman2.jpeg',
+      title: 'Les fruits en folie',
+      description: 'Producteur de fruits',
+      adress: 'Les grandes terres, 69510 MESSIMY',
+      gps: '45.683095, 4.674997',
+    },
+    {
+      id: 1,
+      img: '/media/prodmen2.jpeg',
+      title: 'La ferme Lyonnaise',
+      description: 'Producteur de légumes',
+      adress: '30 avenue de l an 2000, 69290 CRAPONNE',
+      gps: '45.747665, 4.739877',
+    },
+    {
+      id: 1,
+      img: '/media/prodwomen.jpeg',
+      title: 'La ferme d Orliénas',
+      description: 'Producteur de légumes',
+      adress: '91 chemin de la Noyeraie, 69530 Orliénas',
+      gps: '45.666072, 4.717867',
+    },
+  ],
+};
+export default dataKeepers;
--- src/pages/Shopkeepers.js
+++ src/pages/Shopkeepers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import dataKeepers from '../components/DataKeepers';
 
 function Shopkeepers() {
   return (
@@ -12,20 +13,53 @@ function Shopkeepers() {
           className="h-80"
           id="mapid"
           center={[45.764043, 4.835659]}
-          zoom={12}
+          zoom={9.5}
           scrollWheelZoom={false}
         >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.505, -0.09]}>
+          <Marker position={[45.85147, 4.87585]}>
             <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
+              Les jardins du plateau. <br /> Cailloux sur Fontaines.
+            </Popup>
+          </Marker>
+          <Marker position={[45.747665, 4.739877]}>
+            <Popup>
+              La ferme lyonnaise. <br /> Craponne.
+            </Popup>
+          </Marker>
+          <Marker position={[45.683095, 4.674997]}>
+            <Popup>
+              Les fruits en folie. <br /> Messimy.
+            </Popup>
+          </Marker>
+          <Marker position={[45.666072, 4.717867]}>
+            <Popup>
+              La ferme d'Orliénas. <br /> Orliénas.
             </Popup>
           </Marker>
         </MapContainer>
       </div>
+      <section className="py-4 container flex justify-between">
+        <div className="">
+          {dataKeepers.cardData.map((item) => {
+            return (
+              <div className="mx-0 mb-4">
+                <div className="p-0 overflow-hidden h-100 shadow-lg bg-background bg-opacity-60 rounded-md">
+                  <img src={item.img} alt="" className="card-img-top" />
+                  <div className="card-body">
+                    <h2 className="keeper-title">{item.title}</h2>
+                    <p className="keeper-text">{item.description}</p>
+                    <p className="keeper-adress">{item.adress}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
