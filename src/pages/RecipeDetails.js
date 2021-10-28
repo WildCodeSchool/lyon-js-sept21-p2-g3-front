3bf3868e789a5d8f3c811052506b01ecd751168b
solene Verrier <sosso711@hotmail.com>
2021-10-28 10:01:16 +0200

    align and fontsize div time

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import BlenderIcon from '@mui/icons-material/Blender';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
 import Ingredients from '../components/Ingredients';
 
 const RecipeDetails = () => {
@@ -12,16 +13,19 @@ const RecipeDetails = () => {
         className="h-auto w-screen opacity-50"
       />
       <h1 className="text-center m-2">tittle</h1>
-      <div className="flex flex-row justify-around bg-green-200 rounded-lg m-5">
-        <div className="flex flex-row">
-          <BlenderIcon />
-          <div> Temps</div>
+      <div className="flex flex-row justify-around bg-green-200 rounded-lg m-5 h-9">
+        <div className="flex flex-row text-xs">
+          <BlenderIcon fontSize="small" className="m-1" />
+          <div className="m-2"> Temps</div>
         </div>
-        <div className="flex flex-row ">
-          <AccessTimeIcon />
-          <div>Temps</div>
+        <div className="flex flex-row text text-xs">
+          <AccessTimeIcon fontSize="small" className="m-1" />
+          <div className="m-2">Temps</div>
         </div>
-        <div className="" />
+        <div className="flex flex-row text-xs">
+          <div className="m-2">Difficulté </div>
+          <RestaurantIcon fontSize="small" className="m-1" />
+        </div>
       </div>
       <div className="flex justify-center">
         <div className="bg-green-500 h-5 w-1/3 rounded-lg flex justify-center">
