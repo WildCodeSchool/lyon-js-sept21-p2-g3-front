c5e77ebf7c7bd5f10b08f6ca2889e49b23c05808
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-11-04 11:22:48 +0100

    Changed pictures

--- src/pages/Shopkeepers.js
+++ src/pages/Shopkeepers.js
@@ -6,6 +6,85 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 function Shopkeepers() {
   const [dataShopkeepers, setDataShopkeepers] = useState([]);
 
+  const imageProduction = [
+    {
+      code: '10.71',
+      urlImg: 'media/boulangerie.jpeg',
+    },
+    {
+      code: '10.71.1',
+      urlImg: 'media/boulangerie.jpeg',
+    },
+    {
+      code: '10.71.11',
+      urlImg: 'media/boulangerie.jpeg',
+    },
+    {
+      code: '46.34.12',
+      urlImg: 'media/blanc-rouge-vin.jpeg',
+    },
+    {
+      code: '10.72.12',
+      urlImg: 'media/viennoiseries.jpeg',
+    },
+    {
+      code: '46.37',
+      urlImg: 'media/i101986-cafe-nu.jpeg',
+    },
+    {
+      code: '11.07.19',
+      urlImg: 'media/blanc-rouge-vin.jpeg',
+    },
+    {
+      code: '01.47.12',
+      urlImg: 'media/oeufs.jpeg',
+    },
+    {
+      code: '47.00.12',
+      urlImg: 'media/légumes.jpeg',
+    },
+    {
+      code: '47.00.13',
+      urlImg: 'media/viande.jpeg',
+    },
+    {
+      code: '10.82.2',
+      urlImg: 'media/chocolat.jpeg',
+    },
+    {
+      code: '01.19.10.11',
+      urlImg: 'media/légumes.jpeg',
+    },
+    {
+      code: '46.21.11',
+      urlImg: 'media/légumes.jpeg',
+    },
+    {
+      code: '10.71.12',
+      urlImg: 'media/viennoiseries.jpeg',
+    },
+    {
+      code: '47.00.11',
+      urlImg: 'media/légumes.jpeg',
+    },
+    {
+      code: '01.27.12',
+      urlImg: 'media/i101986-cafe-nu.jpeg',
+    },
+    {
+      code: '47',
+      urlImg: 'media/viennoiseries.jpeg',
+    },
+    {
+      code: "GP.Produits.d'épicerie",
+      urlImg: 'media/epicerie.jpeg',
+    },
+    {
+      code: '',
+      urlImg: 'media/légumes.jpeg',
+    },
+  ];
+
   const getDataShopkeepers = () => {
     axios
       .get(
@@ -64,8 +143,25 @@ function Shopkeepers() {
             return (
               <div className="mx-0 mb-8 text-center">
                 <div className="p-0 overflow-hidden h-100 shadow-lg bg-background bg-opacity-60 rounded-md">
-                  {/* <img src={item.img} alt="" className="card-img-top w-full" /> */}
                   <div className="card-body">
+                    <img
+                      src={
+                        // imageProduction[0].urlImg
+                        // console.log(
+                        //   'tab src : ',
+                        imageProduction.filter((prod) => {
+                          if (
+                            item.productions.length !== 0 &&
+                            prod.code === item.productions[0].code
+                          ) {
+                            return prod;
+                          }
+                          return ' ';
+                        })[0].urlImg
+                      }
+                      alt=""
+                    />
+
                     <h2 className="keeper-title">{item.raisonSociale}</h2>
                     {/* <p className="keeper-text">{item.categories}</p> */}
                     <p className="keeper-adress">
