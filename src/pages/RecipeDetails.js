ee14d56cabf5157075d92c948771171911b3b55c
Yannis <yannis.barba.dev@gmail.com>
2021-11-10 10:45:42 +0100

    add the like, addToPlanning and share icons and a container for them

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,11 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { Avatar, CircularProgress } from '@mui/material';
+import { useParams, Link } from 'react-router-dom';
+import { Avatar, Button, CircularProgress } from '@mui/material';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import ShareIcon from '@mui/icons-material/Share';
+import FavoritesContext from '../contexts/FavoritesContexts';
+import MyFoodAPI from '../MyFoodAPI';
 
 const RecipeDetails = () => {
   const { id } = useParams();
   const [recipe, setRecipe] = useState();
+
+  const { favoritesId, setFavoritesId } = useContext(FavoritesContext);
+  const [isFavorite, setIsFavorite] = useState(favoritesId.includes(id));
 
   const getRecipe = () => {
     axios
@@ -25,21 +34,63 @@ const RecipeDetails = () => {
   }
 
   return (
-    <div className="pb-20">
-      <img className="w-screen" src={recipe.image} alt={recipe.label} />
-      <h2> {recipe.label} </h2>
-      <ul className="flex flex-col gap-2 ml-2 mt-4">
-        {recipe.ingredients.map((ingredient) => {
-          return (
-            <li className="flex flex-row gap-2">
-              <Avatar src={ingredient.image} alt={ingredient.food} />{' '}
-              {`${ingredient.food} ${ingredient.quantity} ${ingredient.measure}`}
-            </li>
-          );
-        })}
-      </ul>
-      <a href={recipe.url}> Voir la Recette détaillée </a>
-    </div>
+    <>
+      <div id="page" className="flex flex-col gap-2 items-center pb-24">
+        <div
+          id="share-like-addToPlanning"
+          className="flex flex-col fixed top-1/2 right-2"
+        >
+          <div
+            className="flex items-center justify-center bg-recipeWhite z-20 left-5 -top-8 w-16 h-16 rounded-full"
+            onClick={() => {
+              setIsFavorite(!isFavorite);
+              MyFoodAPI.post(`/favorites/${id}`, {
+                isfavorite: isFavorite,
+              }).then(() => {
+                if (!isFavorite) {
+                  setFavoritesId([...favoritesId, id]);
+                } else {
+                  const newFavoritesId = favoritesId.filter((i) => i !== id);
+                  setFavoritesId(newFavoritesId);
+                }
+              });
+            }}
+          >
+            {' '}
+            {isFavorite ? (
+              <FavoriteIcon sx={{ fontSize: 45, color: '#DD7230' }} />
+            ) : (
+              <FavoriteBorderIcon sx={{ fontSize: 45, color: '#DD7230' }} />
+            )}{' '}
+          </div>{' '}
+          <Link to={`/addtoplanning/${id}`}>
+            <div className="flex items-center justify-center bg-recipeWhite z-20 right-5 -top-8 w-16 h-16 rounded-full">
+              {' '}
+              <AddBoxIcon sx={{ fontSize: 45, color: '#DD7230' }} />{' '}
+            </div>{' '}
+          </Link>
+          <div>
+            <ShareIcon />
+          </div>
+        </div>
+        <img className="w-screen" src={recipe.image} alt={recipe.label} />
+        <h2> {recipe.label} </h2>
+        <ul className="grid grid-cols-2 w-screen gap-2 ml-4 mt-4 pb-6">
+          {recipe.ingredients.map((ingredient) => {
+            return (
+              <li className="flex flex-row gap-2">
+                <Avatar src={ingredient.image} alt={ingredient.food} />{' '}
+                {`${ingredient.food}`}
+              </li>
+            );
+          })}
+        </ul>
+        <Button variant="contained" href={recipe.url}>
+          {' '}
+          Recette détaillée{' '}
+        </Button>
+      </div>
+    </>
   );
 };
 
