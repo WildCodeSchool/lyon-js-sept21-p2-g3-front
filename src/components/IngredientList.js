9f7742c883f35c4883324f76c2a8f63ce5252a18
Yannis <yannis.barba.dev@gmail.com>
2021-11-18 11:40:03 +0100

    draw Button only if the ingredient list is not empty

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,4 +1,3 @@
-/* eslint-disable*/
 import React, { useContext, useState } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -12,13 +11,11 @@ import AddToShoppingListContext from '../contexts/AddToShoppingListContext';
 import MyFoodAPI from '../MyFoodAPI';
 
 export default function IngredientList() {
-  const { shoppingList, setShoppingList, getShoppingList } = useContext(
+  const { shoppingList, getShoppingList } = useContext(
     AddToShoppingListContext
   );
-  // const { shoppingList, setShoppingList } = useContext(AddToShoppingListContext);
-  const [ingredientToDelete, setIngredientToDelete] = useState([]);
 
-  // const { listPlanning } = React.useContext();
+  const [ingredientToDelete, setIngredientToDelete] = useState([]);
 
   const handleToggle = (value) => {
     console.log('ingredient to delete before: ', ingredientToDelete);
@@ -32,18 +29,6 @@ export default function IngredientList() {
     }
     console.log('ingredient to delete after : ', ingredientToDelete);
   };
-
-  // const handleToggle = (value) => () => {
-  //   const currentIndex = checked.indexOf(value);
-  //   const newChecked = [...checked];
-
-  //   if (currentIndex === -1) {
-  //     newChecked.push(value);
-  //   } else {
-  //     newChecked.splice(currentIndex, 1);
-  //   }
-  //   setChecked(newChecked);
-  // };
 
   if (!shoppingList) {
     return <CircularProgress />;
@@ -104,29 +89,34 @@ export default function IngredientList() {
             </ListItem>
           );
         })}
-        <Button
-          onClick={() => {
-            console.log('ingredient send : ', ingredientToDelete);
-            MyFoodAPI.delete('/shopping-list', {
-              data: {
-                ingredientToDelete,
-              },
-            }).then(() => {
-              getShoppingList();
-            });
-          }}
-          type="submit"
-          variant="raised"
-          sx={{
-            color: '#FDB500',
-            bgcolor: '#2E1F27',
-            padding: 2,
-            marginTop: 2,
-            fontWeight: 'bold',
-          }}
-        >
-          DELETE ITEMS
-        </Button>
+
+        {shoppingList.length !== 0 ? (
+          <Button
+            onClick={() => {
+              console.log('ingredient send : ', ingredientToDelete);
+              MyFoodAPI.delete('/shopping-list', {
+                data: {
+                  ingredientToDelete,
+                },
+              }).then(() => {
+                getShoppingList();
+              });
+            }}
+            type="submit"
+            variant="raised"
+            sx={{
+              color: '#FDB500',
+              bgcolor: '#2E1F27',
+              padding: 2,
+              marginTop: 2,
+              fontWeight: 'bold',
+            }}
+          >
+            DELETE ITEMS
+          </Button>
+        ) : (
+          'Aucun ingrédient dans la liste ! '
+        )}
       </List>
     </>
   );
